2947da6127d0e52a8f81004aa16a2a7267263981
fionzheng <fionzheng0@gmail.com>
2021-01-08 17:35:17 -0500

    changes to app.js

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const { MongoClient } = require("mongodb")
 
 const app = express();
 const PORT = process.env.PORT || 8888;
@@ -28,3 +29,45 @@ app.use('/standby', standby)
 
 app.listen(PORT);
 console.log('Listening on http://localhost:' + PORT)
+
+MongoClient.connect("mongodb+srv://doraemon:Fion2002@cluster0.kssuc.mongodb.net/myspotify?authSource=admin&replicaSet=atlas-nimc8j-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+{useNewUrlParser: true})
+.then(client => {
+const db = client.db('Playlist');
+const collection = db.collection('playlists');
+app.locals.collection = collection;
+app.listen(port, () => console.info(`API running on port ${port}`));
+
+}).catch (error => console.error(error));
+
+//addSongsFirst
+app.get('/addSongsFirst', (req, res) => {
+    const firstaddedsongs = addSongsFirst(playlistID, access_token)
+    res.status(200).send(firstaddedsongs)
+});
+
+//playlistsOn
+app.get('/playlistsOn', (req, res) => {
+    const onPlaylists = playlistsOn(access_token)
+    res.status(200).send(onPlaylists)
+
+//checkPlaylistSongs
+app.get('/checkPlaylistSongs', (req, res) => {
+    const checkPlaylists = checkPlaylistSongs(playlistId, access_token)
+    res.status(200).send(checkPlaylists)
+
+//findNewSongInfo
+app.get('/findNewSongInfo', (req, res) => {
+    const songInfo = findNewSongInfo(songID, access_token)
+    res.status(200).send(songInfo)
+
+//updateSongs
+app.get('/updateSongs', (req, res) => {
+    const updatedSongs = updateSongs(playlistID, access_token)
+    res.status(200).send(updatedSongs)
+
+//createMultiplePlaylists
+app.get('/createMultiplePlaylists', (req, res) => {
+    const MultiplePlaylists = createMultiplePlaylists()
+    res.status(200).send(MultiplePlaylists)
+}
